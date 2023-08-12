d053bec6740a78275cdeca80464162c3e571c025
Birk Jernström <birkjernstrom@gmail.com>
2023-08-12 07:48:02 +0200

    macos: add some default settings

--- macOS/config.sh
+++ macOS/config.sh
@@ -8,3 +8,13 @@ defaults write -g KeyRepeat -int 1         # normal minimum is 2 (30 ms)
 
 # VSCode & Vim
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+# No Desktop icons
+defaults write com.apple.finder CreateDesktop -bool false
+killall Finder
+
+# Autohide Dock
+defaults write com.apple.dock "autohide" -bool true
+# Spaces, no re-arrange
+defaults write com.apple.dock "mru-spaces" -bool false
+killall Dock
