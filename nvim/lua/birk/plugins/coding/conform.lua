48bf2b3d16222abc49fa308284194b0b4cb785c2
Birk Jernström <birkjernstrom@gmail.com>
2023-11-19 23:15:37 +0100

    nvim: conform.nvim

---
+++ nvim/lua/birk/plugins/coding/conform.lua
@@ -0,0 +1,38 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      "<leader>cf",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "[C]ode [F]ormat",
+    },
+  },
+  -- Everything in opts will be passed to setup()
+  opts = {
+    -- Define your formatters
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "ruff_fix", "ruff_format" },
+      javascript = { { "prettierd", "prettier" }, "estlint_d" },
+      typescript = { { "prettierd", "prettier" }, "estlint_d" },
+    },
+    -- Set up format-on-save
+    format_on_save = { timeout_ms = 500, lsp_fallback = true },
+    -- Customize formatters
+    formatters = {
+      shfmt = {
+        prepend_args = { "-i", "2" },
+      },
+    },
+  },
+  init = function()
+    -- If you want the formatexpr, here is the place to set it
+    vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+  end,
+}
