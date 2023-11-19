160cb78291ff476f6356c4945a52b08034baef7c
Birk Jernström <birkjernstrom@gmail.com>
2023-11-19 22:35:37 +0100

    nvim: better lsp setup

--- nvim/lua/birk/plugins/lsp/init.lua
+++ nvim/lua/birk/plugins/lsp/init.lua
@@ -40,9 +40,16 @@ return
           nmap('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
           nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
           nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-          nmap('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+          nmap('<leader>dt', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
           nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
           nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+          -- Diagnostic keymaps
+          nmap('<leader>D', '<cmd>Telescope diagnostics bufnr=0<CR>', '[D]iagnostics of current buffer')
+          nmap('<leader>d', vim.diagnostic.open_float, 'Open floating diagnostic message')
+          nmap('[d', vim.diagnostic.goto_prev, 'Go to previous diagnostic message')
+          nmap(']d', vim.diagnostic.goto_next, 'Go to next diagnostic message')
+          nmap('<leader>q', vim.diagnostic.setloclist, 'Open diagnostics list')
 
           -- See `:help K` for why this keymap
           nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
@@ -50,11 +57,7 @@ return
 
           -- Lesser used LSP functionality
           nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-          nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-          nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-          nmap('<leader>wl', function()
-            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          end, '[W]orkspace [L]ist Folders')
+          nmap('<leader>rs', ':LspRestart<CR>', 'Restart LSP Server')
 
           -- Create a command `:Format` local to the LSP buffer
           vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
@@ -79,10 +82,12 @@ return
         local servers = {
           -- clangd = {},
           -- gopls = {},
-          -- pyright = {},
-          -- rust_analyzer = {},
-          -- tsserver = {},
-          -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+          pyright = {},
+          rust_analyzer = {},
+          tsserver = {},
+          cssls = {},
+          tailwindcss = {},
+          html = { filetypes = { 'html', 'twig', 'hbs'} },
 
           lua_ls = {
             Lua = {
