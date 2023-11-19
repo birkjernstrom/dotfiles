ef79762494f5bbec2f807cddf243de1679da421a
Birk Jernström <birkjernstrom@gmail.com>
2023-11-19 14:31:27 +0100

    oil.nvim: keymap + delete to trash

--- nvim-kickstart/lua/birk/plugins/editor/oil.lua
+++ nvim-kickstart/lua/birk/plugins/editor/oil.lua
@@ -2,8 +2,14 @@ return
   {
     {
       'stevearc/oil.nvim',
-      opts = {},
+      opts = {
+        delete_to_trash = true,
+      },
       -- Optional dependencies
       dependencies = { "nvim-tree/nvim-web-devicons" },
+      --
+      init = function()
+        vim.keymap.set('n', '<leader>o', '<cmd>Oil .<cr>', { desc = '[O]il Open CWD' })
+      end,
     }
   }
