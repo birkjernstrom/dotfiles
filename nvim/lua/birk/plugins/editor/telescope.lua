0cf2e02d08074e553816d3125ea34b731eb8443a
Birk Jernström <birkjernstrom@gmail.com>
2023-11-19 21:34:53 +0100

    nvim: better telescope

--- nvim/lua/birk/plugins/editor/telescope.lua
+++ nvim/lua/birk/plugins/editor/telescope.lua
@@ -56,12 +56,17 @@ return
       config = function()
         -- [[ Configure Telescope ]]
         -- See `:help telescope` and `:help telescope.setup()`
+        local actions = require('telescope.actions')
+
         require('telescope').setup {
           defaults = {
             mappings = {
               i = {
-                ['<C-u>'] = false,
-                ['<C-d>'] = false,
+                -- Josean Martinez
+                -- https://www.youtube.com/watch?v=NL8D8EkphUw&t=17s
+                ['<C-k>'] = actions.move_selection_previous,
+                ['<C-j>'] = actions.move_selection_next,
+                ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist,
               },
             },
           },
@@ -84,13 +89,13 @@ return
         end, { desc = '[/] Fuzzily search in current buffer' })
 
         vim.keymap.set('n', '<leader>gf', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
-        vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-        vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-        vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
-        vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-        vim.keymap.set('n', '<leader>sG', ':LiveGrepGitRoot<cr>', { desc = '[S]earch by [G]rep on Git Root' })
-        vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
-        vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+        vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = '[F]ind [F]iles' })
+        vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = '[F]ind [H]elp' })
+        vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = '[F]ind current [W]ord' })
+        vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = '[F]ind by [G]rep' })
+        vim.keymap.set('n', '<leader>fG', ':LiveGrepGitRoot<cr>', { desc = '[F]ind by [G]rep on Git Root' })
+        vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = '[F]ind [D]iagnostics' })
+        vim.keymap.set('n', '<leader>fr', require('telescope.builtin').resume, { desc = '[F]ind [R]esume' })
       end,
     },
   }
--- nvim/lua/birk/plugins/editor/which-key.lua
+++ nvim/lua/birk/plugins/editor/which-key.lua
@@ -12,7 +12,7 @@ return
           ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
           ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
           ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-          ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+          ['<leader>f'] = { name = '[F]ind', _ = 'which_key_ignore' },
           ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         }
       end,
