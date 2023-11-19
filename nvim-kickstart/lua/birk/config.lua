ce2d83de965863cd662feac5d8162a20adea970a
Birk Jernström <birkjernstrom@gmail.com>
2023-11-19 18:35:56 +0100

    nvim: minor style change

--- nvim-kickstart/lua/birk/config.lua
+++ nvim-kickstart/lua/birk/config.lua
@@ -1,50 +1,49 @@
-local g = vim.g
-local o = vim.o
+local opt = vim.o
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-g.mapleader = ' '
-g.maplocalleader = ' '
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
 
 -- Set highlight on search
-o.hlsearch = false
+opt.hlsearch = false
 
 -- Make line numbers default
 vim.wo.number = true
 
 -- Enable mouse mode
-o.mouse = 'a'
+opt.gmouse = 'a'
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-o.clipboard = 'unnamedplus'
+opt.gclipboard = 'unnamedplus'
 
 -- Enable break indent
-o.breakindent = true
+opt.gbreakindent = true
 
 -- Save undo history
-o.undofile = true
+opt.gundofile = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-o.ignorecase = true
-o.smartcase = true
+opt.gignorecase = true
+opt.gsmartcase = true
 
 -- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
 
 -- Decrease update time
-o.updatetime = 250
-o.timeoutlen = 300
+opt.gupdatetime = 250
+opt.gtimeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-o.completeopt = 'menuone,noselect'
+opt.gcompleteopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
-o.termguicolors = true
+opt.gtermguicolors = true
 
 -- Show relative linenumbers - easier navigation
-o.relativenumber = true
+opt.grelativenumber = true
