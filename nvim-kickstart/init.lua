d5f85be427712484ef5e041036b7a5e284469058
Birk Jernström <birkjernstrom@gmail.com>
2023-11-19 14:09:01 +0100

    nvim-kickstart: move custom func

--- nvim-kickstart/init.lua
+++ nvim-kickstart/init.lua
@@ -22,5 +22,16 @@ require('birk.config')
 require('birk.keymap')
 require('birk.plugin_manager')
 
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
