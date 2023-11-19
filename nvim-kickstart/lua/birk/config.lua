898ae4fabbb71c301c32cfb976e37d68ed47ffef
Birk Jernström <birkjernstrom@gmail.com>
2023-11-19 18:39:37 +0100

    nvim: fix typo

--- nvim-kickstart/lua/birk/config.lua
+++ nvim-kickstart/lua/birk/config.lua
@@ -15,35 +15,35 @@ opt.hlsearch = false
 vim.wo.number = true
 
 -- Enable mouse mode
-opt.gmouse = 'a'
+opt.mouse = 'a'
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-opt.gclipboard = 'unnamedplus'
+opt.clipboard = 'unnamedplus'
 
 -- Enable break indent
-opt.gbreakindent = true
+opt.breakindent = true
 
 -- Save undo history
-opt.gundofile = true
+opt.undofile = true
 
 -- Case-insensitive searching UNLESS \C or capital in search
-opt.gignorecase = true
-opt.gsmartcase = true
+opt.ignorecase = true
+opt.smartcase = true
 
 -- Keep signcolumn on by default
 vim.wo.signcolumn = 'yes'
 
 -- Decrease update time
-opt.gupdatetime = 250
-opt.gtimeoutlen = 300
+opt.updatetime = 250
+opt.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
-opt.gcompleteopt = 'menuone,noselect'
+opt.completeopt = 'menuone,noselect'
 
 -- NOTE: You should make sure your terminal supports this
-opt.gtermguicolors = true
+opt.termguicolors = true
 
 -- Show relative linenumbers - easier navigation
-opt.grelativenumber = true
+opt.relativenumber = true
