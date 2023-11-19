17c56b56e701477657be7915ce8e356e4a417c39
Birk Jernström <birkjernstrom@gmail.com>
2023-11-19 22:01:54 +0100

    nvim: better cmp + dressing ui

--- nvim/lua/birk/plugins/coding/cmp.lua
+++ nvim/lua/birk/plugins/coding/cmp.lua
@@ -8,6 +8,10 @@ return
         'L3MON4D3/LuaSnip',
         'saadparwaiz1/cmp_luasnip',
 
+        -- Buffer & Path
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+
         -- Adds LSP completion capabilities
         'hrsh7th/cmp-nvim-lsp',
 
@@ -17,8 +21,8 @@ return
       config = function()
         -- [[ Configure nvim-cmp ]]
         -- See `:help cmp`
-        local cmp = require 'cmp'
-        local luasnip = require 'luasnip'
+        local cmp = require('cmp')
+        local luasnip = require('luasnip')
         require('luasnip.loaders.from_vscode').lazy_load()
         luasnip.config.setup {}
 
@@ -32,8 +36,8 @@ return
             completeopt = 'menu,menuone,noinsert'
           },
           mapping = cmp.mapping.preset.insert {
-            ['<C-n>'] = cmp.mapping.select_next_item(),
-            ['<C-p>'] = cmp.mapping.select_prev_item(),
+            ['<C-k>'] = cmp.mapping.select_prev_item(),
+            ['<C-j>'] = cmp.mapping.select_next_item(),
             ['<C-d>'] = cmp.mapping.scroll_docs(-4),
             ['<C-f>'] = cmp.mapping.scroll_docs(4),
             ['<C-Space>'] = cmp.mapping.complete {},
@@ -63,6 +67,8 @@ return
           sources = {
             { name = 'nvim_lsp' },
             { name = 'luasnip' },
+            { name = 'buffer' },
+            { name = 'path' },
           },
         }
       end,
