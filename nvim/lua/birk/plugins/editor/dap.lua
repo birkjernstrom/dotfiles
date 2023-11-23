c9b2a5de0983ad99abc1839f7e3e7bcd7dd9f85c
Birk Jernström <birkjernstrom@gmail.com>
2023-11-24 00:13:34 +0100

    nvim: remove lazyvim dap specific config

--- nvim/lua/birk/plugins/editor/dap.lua
+++ nvim/lua/birk/plugins/editor/dap.lua
@@ -106,14 +106,7 @@ return {
   },
 
   config = function()
-    local Config = require 'lazyvim.config'
     vim.api.nvim_set_hl(0, 'DapStoppedLine', { default = true, link = 'Visual' })
-
     require('dap.ext.vscode').load_launchjs()
-
-    for name, sign in pairs(Config.icons.dap) do
-      sign = type(sign) == 'table' and sign or { sign }
-      vim.fn.sign_define('Dap' .. name, { text = sign[1], texthl = sign[2] or 'DiagnosticInfo', linehl = sign[3], numhl = sign[3] })
-    end
   end,
 }
