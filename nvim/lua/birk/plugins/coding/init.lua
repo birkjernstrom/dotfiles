97997dd0fd5031d74fb980d90b22401fc2e29f6a
Birk Jernström <birkjernstrom@gmail.com>
2023-11-23 23:57:17 +0100

    nvim: mini.* plugins

--- nvim/lua/birk/plugins/coding/init.lua
+++ nvim/lua/birk/plugins/coding/init.lua
@@ -1,17 +1,18 @@
-return
+return {
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+  -- mini.* plugins
+  { 'echasnovski/mini.comment', opts = {} },
+  { 'echasnovski/mini.pairs', opts = {} },
+  { 'echasnovski/mini.surround', opts = {} },
+
+  -- Add indentation guides even on blank lines
   {
-    -- Detect tabstop and shiftwidth automatically
-    'tpope/vim-sleuth',
-
-    -- "gc" to comment visual regions/lines
-    { 'numToStr/Comment.nvim', opts = {} },
-
-    -- Add indentation guides even on blank lines
-    {
-      'lukas-reineke/indent-blankline.nvim',
-      -- Enable `lukas-reineke/indent-blankline.nvim`
-      -- See `:help ibl`
-      main = 'ibl',
-      opts = {},
-    },
-  }
+    'lukas-reineke/indent-blankline.nvim',
+    -- Enable `lukas-reineke/indent-blankline.nvim`
+    -- See `:help ibl`
+    main = 'ibl',
+    opts = {},
+  },
+}
