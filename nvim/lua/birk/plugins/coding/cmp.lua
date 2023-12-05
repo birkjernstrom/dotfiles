1c696abece8a5c28c255aa65fdab884025265c85
Birk Jernström <birkjernstrom@gmail.com>
2023-12-06 00:07:13 +0100

    nvim: copilot

--- nvim/lua/birk/plugins/coding/cmp.lua
+++ nvim/lua/birk/plugins/coding/cmp.lua
@@ -1,76 +1,96 @@
-return
+local has_words_before = function()
+  if vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt' then
+    return false
+  end
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match '^%s*$' == nil
+end
+
+return {
+  -- Autocompletion
   {
-    -- Autocompletion
-    {
-      'hrsh7th/nvim-cmp',
-      dependencies = {
-        -- Snippet Engine & its associated nvim-cmp source
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
 
-        -- Buffer & Path
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
+      -- Buffer & Path
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
 
-        -- Adds LSP completion capabilities
-        'hrsh7th/cmp-nvim-lsp',
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
 
-        -- Adds a number of user-friendly snippets
-        'rafamadriz/friendly-snippets',
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+
+      -- GitHub Copilot
+      {
+        'zbirenbaum/copilot-cmp',
+        dependencies = 'copilot.lua',
       },
-      config = function()
-        -- [[ Configure nvim-cmp ]]
-        -- See `:help cmp`
-        local cmp = require('cmp')
-        local luasnip = require('luasnip')
-        require('luasnip.loaders.from_vscode').lazy_load()
-        luasnip.config.setup {}
+    },
+    config = function()
+      -- [[ Configure nvim-cmp ]]
+      -- See `:help cmp`
+      local cmp = require 'cmp'
+      local luasnip = require 'luasnip'
 
-        cmp.setup {
-          snippet = {
-            expand = function(args)
-              luasnip.lsp_expand(args.body)
-            end,
+      require('luasnip.loaders.from_vscode').lazy_load()
+      luasnip.config.setup {}
+
+      local copilot = require 'copilot_cmp'
+      copilot.setup()
+
+      cmp.setup {
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        completion = {
+          completeopt = 'menu,menuone,noinsert',
+        },
+        mapping = cmp.mapping.preset.insert {
+          ['<C-k>'] = cmp.mapping.select_prev_item(),
+          ['<C-j>'] = cmp.mapping.select_next_item(),
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete {},
+          ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
           },
-          completion = {
-            completeopt = 'menu,menuone,noinsert'
-          },
-          mapping = cmp.mapping.preset.insert {
-            ['<C-k>'] = cmp.mapping.select_prev_item(),
-            ['<C-j>'] = cmp.mapping.select_next_item(),
-            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete {},
-            ['<CR>'] = cmp.mapping.confirm {
-              behavior = cmp.ConfirmBehavior.Replace,
-              select = true,
-            },
-            ['<Tab>'] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                cmp.select_next_item()
-              elseif luasnip.expand_or_locally_jumpable() then
-                luasnip.expand_or_jump()
-              else
-                fallback()
-              end
-            end, { 'i', 's' }),
-            ['<S-Tab>'] = cmp.mapping(function(fallback)
-              if cmp.visible() then
-                cmp.select_prev_item()
-              elseif luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
-              else
-                fallback()
-              end
-            end, { 'i', 's' }),
-          },
-          sources = {
-            { name = 'nvim_lsp' },
-            { name = 'luasnip' },
-            { name = 'buffer' },
-            { name = 'path' },
-          },
-        }
-      end,
-    }
-  }
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visile() and has_words_before() then
+              cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
+            elseif cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        },
+        sources = {
+          { name = 'copilot' },
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+        },
+      }
+    end,
+  },
+}
