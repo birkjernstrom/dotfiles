2c9af11b0745639267503db5814d20eb10ba2b75
Birk Jernström <birkjernstrom@gmail.com>
2023-12-19 09:52:07 +0100

    nvim: -indent-blankline

--- nvim/lua/birk/plugins/init.lua
+++ nvim/lua/birk/plugins/init.lua
@@ -1,13 +1,1 @@
-return {
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
-
-  -- Add indentation guides even on blank lines
-  {
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
-}
+return {}
