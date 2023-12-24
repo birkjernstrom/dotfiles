ea6bdd5e0e3634d0e4ce95857ee91230ef3859b7
Birk Jernström <birkjernstrom@gmail.com>
2023-12-25 00:49:07 +0100

    nvim: comment, treesj, dressing

--- nvim/lua/birk/plugins/editing.lua
+++ nvim/lua/birk/plugins/editing.lua
@@ -1,6 +1,28 @@
 return {
+    -- Smart indentation based on buffer, editorconfig etc
     {
         'tpope/vim-sleuth',
         tag = 'v2.0',
+    },
+    -- Ease commenting of code
+    {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end,
+    },
+    -- Split/join blocks of code
+    {
+        'Wansmer/treesj',
+        keys = {
+            '<leader>m',
+        },
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+        },
+        config = function()
+            require('treesj').setup()
+        end,
     }
+
 }
