87ab270002b886fa843f258d4bbebb50da9e4d71
Birk Jernström <birkjernstrom@gmail.com>
2023-12-25 01:01:17 +0100

    nvim: indentation guides

--- nvim/lua/birk/plugins/ui.lua
+++ nvim/lua/birk/plugins/ui.lua
@@ -1,6 +1,45 @@
 return {
+	-- Improve vim.ui interfaces
 	{
 		'stevearc/dressing.nvim',
 		opts = {},
+	},
+	-- Show idenentation guides
+	{
+		'lukas-reineke/indent-blankline.nvim',
+		config = function()
+			local ibl = require 'ibl'
+			local hooks = require 'ibl.hooks'
+
+			hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+				vim.api.nvim_set_hl(0, 'Muted', { fg = '#232137' })
+				vim.api.nvim_set_hl(0, 'Highlighted', { fg = '#6E6A86' })
+			end)
+
+			ibl.setup {
+				indent = {
+					char = '│',
+					tab_char = '│',
+					highlight = { 'Muted' },
+				},
+				scope = { enabled = true, highlight = { 'Highlighted' } },
+				exclude = {
+					filetypes = {
+						'help',
+						'alpha',
+						'dashboard',
+						'neo-tree',
+						'Trouble',
+						'trouble',
+						'lazy',
+						'mason',
+						'notify',
+						'toggleterm',
+						'lazyterm',
+					},
+				},
+			}
+		end,
+		main = 'ibl',
 	}
 }
