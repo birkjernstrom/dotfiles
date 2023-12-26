c1c8ba660407f7c17253cb24b657caf4347526a8
Birk Jernström <birkjernstrom@gmail.com>
2023-12-26 14:27:35 +0100

    nvim: add cmp lsp capabilities

--- nvim/lua/birk/plugins/lsp.lua
+++ nvim/lua/birk/plugins/lsp.lua
@@ -100,6 +100,8 @@ return {
 		})
 
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
 		mason_lspconfig.setup_handlers({
 			function(server_name)
 				require('lspconfig')[server_name].setup({
