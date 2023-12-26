a6bda50da2608da4d07bd3681cca2fc2df0c63d7
Birk Jernström <birkjernstrom@gmail.com>
2023-12-26 23:12:02 +0100

    nvim: conform

---
+++ nvim/lua/birk/plugins/conform.lua
@@ -0,0 +1,55 @@
+return {
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"stylua",
+					"ruff",
+					"black",
+					"prettierd",
+				},
+			})
+		end,
+	},
+	{
+		"stevearc/conform.nvim",
+		dependencies = {},
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				-- Customize or remove this keymap to your liking
+				"<leader>cf",
+				function()
+					require("conform").format({ async = true, lsp_fallback = true })
+				end,
+				mode = "",
+				desc = "Code Format",
+			},
+		},
+		opts = {
+			-- Everything in opts will be passed to setup()
+			formatters_by_ft = {
+				lua = { "stylua" },
+				python = { "ruff", "black" },
+				javascript = { { "prettierd", "prettier" } },
+				typescript = { { "prettierd", "prettier" } },
+				javascriptreact = { { "prettierd", "prettier" } },
+				typescriptreact = { { "prettierd", "prettier" } },
+			},
+			-- Set up format-on-save
+			format_on_save = { timeout_ms = 500, lsp_fallback = true },
+			-- Customize formatters
+			formatters = {
+				shfmt = {
+					prepend_args = { "-i", "2" },
+				},
+			},
+		},
+		init = function()
+			-- If you want the formatexpr, here is the place to set it
+			vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+		end,
+	},
+}
