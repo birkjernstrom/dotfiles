0dd80aaac19b19364186520069467b247e8482da
Birk Jernström <birkjernstrom@gmail.com>
2023-12-27 15:05:21 +0100

    nvim: which-key

---
+++ nvim/lua/birk/plugins/which-key.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 500
+  end,
+  config = function()
+    local wk = require("which-key")
+    wk.setup()
+  end,
+}
