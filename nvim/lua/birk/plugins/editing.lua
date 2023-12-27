cbecda0ac96c4d144b6cb56d5610f7ef2b87b2bc
Birk Jernström <birkjernstrom@gmail.com>
2023-12-27 13:46:54 +0100

    nvim: use treesitter in autopairs

--- nvim/lua/birk/plugins/editing.lua
+++ nvim/lua/birk/plugins/editing.lua
@@ -1,51 +1,51 @@
 return {
-	-- Smart indentation based on buffer, editorconfig etc
-	{
-		"tpope/vim-sleuth",
-		tag = "v2.0",
-	},
-	-- Ease commenting of code
-	{
-		"numToStr/Comment.nvim",
-		dependencies = {
-			"JoosepAlviste/nvim-ts-context-commentstring",
-		},
-		config = function()
-			local ts_context_comment = require("ts_context_commentstring.integrations.comment_nvim")
-			require("Comment").setup({
-				pre_hook = ts_context_comment.create_pre_hook(),
-			})
-		end,
-	},
-	-- Split/join blocks of code
-	{
-		"Wansmer/treesj",
-		keys = {
-			"<leader>cm",
-		},
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-		},
-		config = function()
-			require("treesj").setup()
-		end,
-	},
-	-- Automatically create closing pairs
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		opts = {
-			disable_filetype = { "TelescopePrompt", "vim" },
-		},
-	},
-	{
-		"windwp/nvim-ts-autotag",
-		event = "InsertEnter",
-	},
-	-- Commands to surround selections with characters
-	{
-		"kylechui/nvim-surround",
-		event = "VeryLazy",
-		opts = {},
-	},
+  -- Smart indentation based on buffer, editorconfig etc
+  {
+    "tpope/vim-sleuth",
+    tag = "v2.0",
+  },
+  -- Ease commenting of code
+  {
+    "numToStr/Comment.nvim",
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+    config = function()
+      local ts_context_comment = require("ts_context_commentstring.integrations.comment_nvim")
+      require("Comment").setup({
+        pre_hook = ts_context_comment.create_pre_hook(),
+      })
+    end,
+  },
+  -- Split/join blocks of code
+  {
+    "Wansmer/treesj",
+    keys = {
+      "<leader>cm",
+    },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("treesj").setup()
+    end,
+  },
+  -- Automatically create closing pairs
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {
+      check_ts = true,
+    },
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+  },
+  -- Commands to surround selections with characters
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    opts = {},
+  },
 }
