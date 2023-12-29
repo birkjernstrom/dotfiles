66d2a31c3a524005dc23dbd58088be3b0821c294
Birk Jernström <birkjernstrom@gmail.com>
2023-12-29 20:29:21 +0100

    nvim: harpoon without telescope (again)

--- nvim/lua/birk/plugins/harpoon.lua
+++ nvim/lua/birk/plugins/harpoon.lua
@@ -1,22 +1,3 @@
-local function toggle_telescope(harpoon_files)
-  local conf = require("telescope.config").values
-  local file_paths = {}
-  for _, item in ipairs(harpoon_files.items) do
-    table.insert(file_paths, item.value)
-  end
-
-  require("telescope.pickers")
-    .new({}, {
-      prompt_title = "Harpoon",
-      finder = require("telescope.finders").new_table({
-        results = file_paths,
-      }),
-      previewer = conf.file_previewer({}),
-      sorter = conf.generic_sorter({}),
-    })
-    :find()
-end
-
 return {
   {
     "ThePrimeagen/harpoon",
@@ -32,7 +13,7 @@ return {
         harpoon:list():append()
       end, { desc = "Add file to Harpoon list" })
       vim.keymap.set("n", "<C-b>", function()
-        toggle_telescope(harpoon:list())
+        harpoon.ui:toggle_quick_menu(harpoon:list())
       end, { desc = "Toggle Harpoon list" })
 
       vim.keymap.set("n", "<M-1>", function()
