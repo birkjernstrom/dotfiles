554d0bb9f6479f1fdce6c26678388fc52b813989
Birk Jernström <birkjernstrom@gmail.com>
2024-06-08 15:38:47 +0200

    nvim: improve copilot

--- nvim/lua/birk/plugins/copilot.lua
+++ nvim/lua/birk/plugins/copilot.lua
@@ -1,13 +1,38 @@
+-- Copilot
 return {
-  'zbirenbaum/copilot.lua',
-  cmd = 'Copilot',
-  build = ':Copilot auth',
-  event = 'InsertEnter',
-  opts = {
-    suggestion = { enabled = false },
-    panel = { enabled = false },
-    filetypes = {
-      markdown = true,
-    },
-  },
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      suggestion = { enabled = false },
+      panel = {
+        enabled = true,
+        auto_refresh = false,
+        keymap = {
+          jump_prev = "[[",
+          jump_next = "]]",
+          accept = "<CR>",
+        },
+        layout = {
+          position = "bottom",
+          ratio = 0.4,
+        },
+      },
+      filetypes = {
+        yaml = false,
+        markdown = false,
+        help = false,
+        gitcommit = false,
+        gitrebase = false,
+        hgcommit = false,
+        svn = false,
+        cvs = false,
+        ["."] = false,
+      },
+    })
+
+    vim.keymap.set("n", "<leader>co", require("copilot.panel").open, { desc = "[copilot] Open Panel" })
+  end,
 }
